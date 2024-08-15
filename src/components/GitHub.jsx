3cd4fde8ae304fa0c61249fbc96a21a5b641063e
Rodrigo312-dev <rodrigomachacajimenez312@gmail.com>
2024-08-14 21:01:16 -0400

    react components

---
+++ src/components/GitHub.jsx
@@ -0,0 +1,16 @@
+const GitHub = () => {
+    return (
+        <>
+            <a
+                className="App-link"
+                href="https://github.com/Rodrigo312-dev/dev-frontend-react"
+                target="_blank"
+                rel="noopener noreferrer"
+            >
+                Mi Proyecto React GitHub
+            </a>
+        </>
+    );
+};
+
+export default GitHub;
