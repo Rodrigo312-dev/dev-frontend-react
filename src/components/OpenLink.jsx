96371c2cc8fa92ea0753fc798ec167636f206df1
Rodrigo312-dev <rodrigomachacajimenez312@gmail.com>
2024-08-14 21:18:19 -0400

    react propts

---
+++ src/components/OpenLink.jsx
@@ -0,0 +1,16 @@
+const OpenLink = ({ title, url }) => {
+    return (
+        <>
+            <a
+                className="App-link"
+                href={url}
+                target="_blank"
+                rel="noopener noreferrer"
+            >
+                {title}
+            </a>
+        </>
+    );
+};
+
+export default OpenLink;
--- src/screens/Default.jsx
+++ src/screens/Default.jsx
@@ -1,7 +1,7 @@
-import Logo from '../components/Logo';
-import Description from '../components/Description';
-import Content from '../components/Content';
-import GitHub from '../components/GitHub';
+import Logo from './_components/Logo';
+import Description from './_components/Description';
+
+import OpenLink from '../components/OpenLink';
 
 const Default = () => {
     return (
@@ -9,8 +9,9 @@ const Default = () => {
             <header className="App-header">
                 <Logo />
                 <Description />
-                <Content />
-                <GitHub/>
+
+                <OpenLink title="Learn React" url="https://reactjs.org"/>
+                <OpenLink title="Mi Proyecto React GitHub" url="https://github.com/Rodrigo312-dev/dev-frontend-react"/>
             </header>
         </>
     );
