0789e90fae9ab21a82138e6882009e690f3cb4c3
Rodrigo312-dev <rodrigomachacajimenez312@gmail.com>
2024-08-22 21:22:28 -0400

    modal Form

--- src/screens/Forms/LoginForm.jsx
+++ src/screens/Forms/LoginForm.jsx
@@ -1,47 +1,82 @@
 import { useSelector, useDispatch } from "react-redux";
 import useForm from "../../hooks/useForm";
 import { saveFormData } from "../../redux/form/formActions";
+import { motion } from 'framer-motion';
+import ModalInfo from "../../components/ModalInfo";
+import { useState } from "react";
 
 const LoginForm = () => {
-    const[values, handleChange] = useForm({username: '', email: ''})
-    const form = useSelector (state => state.form);
+    const [values, handleChange] = useForm({ username: '', email: '',password: '' });
+    const [showModalinfo, setShowModalInfo] = useState(false);
+    const form = useSelector(state => state.form);
     const dispatch = useDispatch();
 
-    const handleSubmit = (event) =>{
+    const handleSubmit = (event) => {
         event.preventDefault();
         console.log(values);
         dispatch(saveFormData(values));
+        setShowModalInfo(true);
     }
-    return(
-        <>
-            <h1>COMPONENTE LOGIN</h1>
-            <br />
-            <form onSubmit={handleSubmit}>
-                <h5> UserName: {form.formData.username}</h5>
-                <h5> Email: {form.formData.email}</h5>
-                <div>
-                    <label htmlFor="username">Username</label>
-                    <input 
-                        type="text" 
-                        id="username" 
-                        name="username"
-                        value={values.username}
-                        onChange={handleChange}
-                    />
-                </div>
-                <div>
-                    <label htmlFor="email">Email</label>
-                    <input 
-                        type="email" 
-                        id="email" 
-                        name="email"
-                        value={values.email}
-                        onChange={handleChange}
-                    />
-                </div>
-                <button type="submit">Submit</button>
-            </form>
-        </>
+
+    const hideModalInfo = () => {
+        setShowModalInfo(false);
+    };
+    const modalInfoShow = () => {
+        setShowModalInfo(true);
+    };
+
+    return (
+        <motion.div
+            initial={{opacity: 0, y: -70}}
+            animate={{opacity: 1, y: 0}}
+            transition={{duration: 1}}
+        >
+            <ModalInfo 
+                    visible={showModalinfo}
+                    message= {values.username + " Bienvenid@ al Módulo 7"}
+                    onClose={hideModalInfo}
+                />
+            <div className="container">
+                <form onSubmit={handleSubmit}>
+                    <h5> UserName: {form.formData.username}</h5>
+                    <h5> Email: {form.formData.email}</h5>
+                    <div>
+                        <label htmlFor="username">Username</label>
+                        <input 
+                            type="text" 
+                            id="username" 
+                            name="username"
+                            value={values.username}
+                            onChange={handleChange}
+                        />
+                    </div>
+                    <div>
+                        <label htmlFor="email">Email</label>
+                        <input 
+                            type="email" 
+                            id="email" 
+                            name="email"
+                            value={values.email}
+                            onChange={handleChange}
+                        />
+                    </div>
+                    <div>
+                        <label htmlFor="password">Contraseña</label>
+                        <input 
+                            type="password" 
+                            id="password" 
+                            name="password"
+                            value={values.password}
+                            onChange={handleChange}
+                        />
+                    </div>
+                    <div className="button-container">
+                        <button type="submit">Submit</button>
+                        <button type="submit" onClick={modalInfoShow}>Mostrar Modal</button>
+                    </div>
+                </form>
+            </div>
+        </motion.div>
     );
 };
 
